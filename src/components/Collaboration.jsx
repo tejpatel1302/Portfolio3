74651de671718e86f0fa29bbd03c577f41fcec5a
tejpatel1302 <tgp02tp@gmail.com>
2024-06-15 21:24:21 +0530

    first commit

---
+++ src/components/Collaboration.jsx
@@ -0,0 +1,13 @@
+import { brainwaveSymbol, check } from "../assets";
+import { collabApps, collabContent, collabText } from "../constants";
+import Button from "./Button";
+import Section from "./Section";
+import { LeftCurve, RightCurve } from "./design/Collaboration";
+
+const Collaboration = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default Collaboration;
---
+++ src/constants/index.js
@@ -0,0 +1,329 @@
+import {
+  benefitIcon1,
+  benefitIcon2,
+  benefitIcon3,
+  benefitIcon4,
+  benefitImage2,
+  chromecast,
+  disc02,
+  discord,
+  discordBlack,
+  facebook,
+  figma,
+  file02,
+  framer,
+  homeSmile,
+  instagram,
+  notification2,
+  notification3,
+  notification4,
+  notion,
+  photoshop,
+  plusSquare,
+  protopie,
+  raindrop,
+  recording01,
+  recording03,
+  roadmap1,
+  roadmap2,
+  roadmap3,
+  roadmap4,
+  searchMd,
+  slack,
+  sliders04,
+  telegram,
+  twitter,
+  yourlogo,
+  react,
+ nextjs,
+ nestjs,
+ js,
+ tailwindcss,
+ laravel,
+phase14
+} from "../assets";
+
+export const navigation = [
+  {
+    id: "0",
+    title: "About",
+    url: "#about",
+  },
+  {
+    id: "1",
+    title: "Projects",
+    url: "#projects",
+  },
+  {
+    id: "2",
+    title: "Tech Stack",
+    url: "#techstack",
+  },
+  {
+    id: "3",
+    title: "Approach",
+    url: "#myapproach",
+  },
+ 
+];
+
+export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
+
+export const notificationImages = [notification4, notification3, notification2];
+
+export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
+
+export const brainwaveServices = [
+  "Photo generating",
+  "Photo enhance",
+  "Seamless Integration",
+];
+
+export const brainwaveServicesIcons = [
+  recording03,
+  recording01,
+  disc02,
+  chromecast,
+  sliders04,
+];
+
+export const roadmap = [
+  {
+    id: "0",
+    title: "Planning & Strategy",
+    text: "We'll collaborate to map out your website's goals, target audience, and key functionalities. We'll discuss things like site structure, navigation, and content requirements.",
+    date: "Phase 1",
+    status: "done",
+    imageUrl: phase14,
+    colorful: true,
+  },
+  {
+    id: "1",
+    title: "Design",
+    text: "We create initial sketches and prototypes of the website design, ensuring that it aligns with the planning and strategy phase.",
+    date: "Phase 2",
+    status: "progress",
+    imageUrl: roadmap2,
+  },
+  {
+    id: "2",
+    title: "Development",
+    text: "Based on the approved design, I'll translate everything into functional code, building your website from the ground up. I keep you updated every step of the way.",
+    date: "Phase 3",
+    status: "progress",
+    imageUrl: roadmap3,
+  },
+  {
+    id: "3",
+    title: "Testing & Launch",
+    text: "A detailed test plan is created, outlining the strategy, objectives, resources, schedule, and scope of testing. Once testing is complete, we proceed to launch the website.",
+    date: "Phase 4",
+    status: "done",
+    imageUrl: phase14,
+  },
+];
+
+
+export const collabText =
+  "Languages and Markup: HTML5, CSS3, JavaScript (ES6+)                                                Frameworks/Libraries: React.js, Next.js";
+  export const collabText2 = 
+  "Languages: JavaScript (Node.js), PHP                                                  Frameworks/Libraries: Express.js (Node.js), Laravel (PHP), Nestjs";
+  export const collabText3 = 
+  "SQL: MySQL,PostgreSQL, SQLite NoSQL: MongoDB, Firebase";
+export const collabContent = [
+  {
+    id: "0",
+    title: "Front-End Development",
+    text: collabText,
+  },
+  {
+    id: "1",
+    title: "Back-End Development",
+    text: collabText2,
+  },
+  {
+    id: "2",
+    title: "Databases",
+    text: collabText3,
+  },
+];
+
+export const collabApps = [
+  {
+    id: "0",
+    title: "Figma",
+    icon: figma,
+    width: 26,
+    height: 36,
+  },
+  {
+    id: "1",
+    title: "Notion",
+    icon: react,
+    width: 34,
+    height: 36,
+  },
+  {
+    id: "2",
+    title: "Discord",
+    icon: nextjs,
+    width: 36,
+    height: 28,
+  },
+  {
+    id: "3",
+    title: "Slack",
+    icon: nestjs,
+    width: 34,
+    height: 35,
+  },
+  {
+    id: "4",
+    title: "Photoshop",
+    icon: js,
+    width: 34,
+    height: 34,
+  },
+  {
+    id: "5",
+    title: "Protopie",
+    icon: laravel,
+    width: 34,
+    height: 34,
+  },
+  {
+    id: "6",
+    title: "Framer",
+    icon: tailwindcss,
+    width: 26,
+    height: 34,
+  },
+  {
+    id: "7",
+    title: "Raindrop",
+    icon: raindrop,
+    width: 38,
+    height: 32,
+  },
+];
+
+export const pricing = [
+  {
+    id: "1",
+    title: "simplici",
+    description: "Frontend technology: Next.js, Tailwind CSS | Backend technology: NestJS",
+    prices: "https://simplici.io/",
+    features: [
+      
+    ],
+  },
+  {
+    id: "2",
+    title: "Jupiter Health",
+    description: "Frontend technology: Next.js, Tailwind CSS | Backend technology: NestJS",
+    prices: 'https://www.jupiter.ca/',
+    features: [
+      
+    ],
+  },
+  {
+    id: "0",
+    title: "RxToYou",
+    description: "Frontend technology: Next.js, Tailwind CSS | Backend technology: NestJS",
+    prices: "http://node-modules.com/",
+    features: [
+  
+    ],
+  },
+];
+
+export const benefits = [
+  {
+    id: "0",
+    title: "I prioritize client collaboration, fostering open communication",
+    text: "",
+    backgroundUrl: "./src/assets/benefits/card-1.svg",
+    iconUrl: benefitIcon1,
+    imageUrl: benefitImage2,
+  },
+  {
+    id: "1",
+    title: "I'm very flexible with time zone communications",
+    text: "",
+    backgroundUrl: "./src/assets/benefits/card-2.svg",
+    iconUrl: benefitIcon2,
+    imageUrl: benefitImage2,
+    light: true,
+  },
+  {
+    id: "2",
+    title: "Tech enthusiast with a passion for development.",
+    text: "",
+    backgroundUrl: "./src/assets/benefits/card-3.svg",
+    iconUrl: benefitIcon3,
+    imageUrl: benefitImage2,
+  },
+  {
+    id: "3",
+    title: "I constantly try to improve My tech stack",
+    text: "",
+    backgroundUrl: "./src/assets/benefits/card-4.svg",
+    iconUrl: benefitIcon4,
+    imageUrl: benefitImage2,
+    light: true,
+  },
+  {
+    id: "4",
+    title: "Committed to delivering high-quality code",
+    text: "",
+    backgroundUrl: "./src/assets/benefits/card-4.svg",
+    iconUrl: benefitIcon1,
+    imageUrl: benefitImage2,
+    light: true,
+  },
+
+  {
+    id: "5",
+    title: "Experienced in agile methodologies and project management",
+    text: "",
+    backgroundUrl: "./src/assets/benefits/card-4.svg",
+    iconUrl: benefitIcon2,
+    imageUrl: benefitImage2,
+    light: true,
+  },
+
+
+];
+
+export const socials = [
+  {
+    id: "0",
+    title: "Discord",
+    iconUrl: discordBlack,
+    url: "#",
+  },
+  {
+    id: "1",
+    title: "Twitter",
+    iconUrl: twitter,
+    url: "#",
+  },
+  {
+    id: "2",
+    title: "Instagram",
+    iconUrl: instagram,
+    url: "#",
+  },
+  {
+    id: "3",
+    title: "Telegram",
+    iconUrl: telegram,
+    url: "#",
+  },
+  {
+    id: "4",
+    title: "Facebook",
+    iconUrl: facebook,
+    url: "#",
+  },
+];
